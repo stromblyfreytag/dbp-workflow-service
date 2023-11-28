c6086dca66f71830f8f1666614be31a75a5003fd
Scot Freytag <stromblyfreytag@trustwave.com>
2023-11-28 11:07:30 -0600

    initial code

---
+++ src/main/java/com/trustwave/dbpworkflow/DbpWorkflowServiceApplication.java
@@ -0,0 +1,13 @@
+package com.trustwave.dbpworkflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbpWorkflowServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbpWorkflowServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trustwave/dbpworkflow/DbpWorkflowServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trustwave.dbpworkflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbpWorkflowServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

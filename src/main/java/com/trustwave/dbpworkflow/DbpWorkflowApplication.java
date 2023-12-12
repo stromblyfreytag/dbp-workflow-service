f54d1ff5463807f5cc82c9b3880b6c1696874a60
Scot Freytag <stromblyfreytag@trustwave.com>
2023-12-12 14:38:34 -0600

    Creating base actions and BPMN workflow

--- src/main/java/com/trustwave/dbpworkflow/DbpWorkflowApplication.java
+++ src/main/java/com/trustwave/dbpworkflow/DbpWorkflowApplication.java
@@ -11,27 +11,27 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(proxyBeanMethods = false)
 public class DbpWorkflowApplication {
-//	@Autowired
-//	private RuntimeService runtimeService;
-//
-//	@Autowired
-//	private TaskService taskService;
+	@Autowired
+	private RuntimeService runtimeService;
+
+	@Autowired
+	private TaskService taskService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DbpWorkflowApplication.class, args);
 	}
-//	@Bean
-//	public CommandLineRunner init( final RuntimeService runtimeService,
-//			final TaskService taskService) {
-//
-//		return new CommandLineRunner() {
-//			@Override
-//			public void run(String... strings) throws Exception {
-//				System.out.println("Number of tasks : " + taskService.createTaskQuery().count());
-//				runtimeService.startProcessInstanceByKey("oneTaskProcess");
-//				System.out.println("Number of tasks after process start: "
-//						+ taskService.createTaskQuery().count());
-//			}
-//		};
-//	}
+	@Bean
+	public CommandLineRunner init( final RuntimeService runtimeService,
+			final TaskService taskService) {
+
+		return new CommandLineRunner() {
+			@Override
+			public void run(String... strings) throws Exception {
+				System.out.println("Number of tasks : " + taskService.createTaskQuery().count());
+				runtimeService.startProcessInstanceByKey("demo");
+				System.out.println("Number of tasks after process start: "
+						+ taskService.createTaskQuery().count());
+			}
+		};
+	}
 }
---
+++ src/main/java/com/trustwave/dbpworkflow/task/MailAction.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2023 Trustwave Holdings, Inc.
+ * All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information
+ * of Trustwave Holdings, Inc.  Use of this software is governed by
+ * the terms and conditions of the license statement and limited
+ * warranty furnished with the software.
+ *
+ * IN PARTICULAR, YOU WILL INDEMNIFY AND HOLD TRUSTWAVE HOLDINGS INC.,
+ * ITS RELATED COMPANIES AND ITS SUPPLIERS, HARMLESS FROM AND AGAINST
+ * ANY CLAIMS OR LIABILITIES ARISING OUT OF OR RESULTING FROM THE USE,
+ * MODIFICATION, OR DISTRIBUTION OF PROGRAMS OR FILES CREATED FROM,
+ * BASED ON, AND/OR DERIVED FROM THIS SOURCE CODE FILE.
+ */
+package com.trustwave.dbpworkflow.task;
+
+import org.flowable.engine.delegate.DelegateExecution;
+import org.flowable.engine.delegate.JavaDelegate;
+
+/**
+ * -- TODO add description here
+ *
+ * <pre>
+ * Copyright (c) 2023 Trustwave Holdings, Inc.
+ * All rights reserved.
+ * </pre>
+ *
+ * @author sfreytag
+ */
+public class MailAction implements JavaDelegate {
+    public void execute(DelegateExecution execution) {
+        System.out.println("MailAction Action");
+    }
+}
